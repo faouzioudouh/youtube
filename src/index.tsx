079134830b06cee5054234d15855b2a07153176a
Faouzi OUDOUH <faouzi.oudouh@gmail.com>
2017-10-25 23:13:52 +0100

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 import * as ReactDOM from 'react-dom';
 import InstantYoutube from './InstantYoutube';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(
   InstantYoutube,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
