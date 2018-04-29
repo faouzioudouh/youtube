ac63c1f63549c4ddd13778e278cf647f365cad1b
faouzioudouh <faouzi.oudouh@gmail.com>
2018-04-29 14:27:15 +0200

    Remove unwated file

--- src/icons.font.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    files: ['src/icons/*.svg'],
-    fontName: 'Crema Glyphs',
-    classPrefix: 'Glyph--',
-    baseClass: 'Glyph',
-    fixedWidth: true,
-    types: ['woff'],
-  };
-  
